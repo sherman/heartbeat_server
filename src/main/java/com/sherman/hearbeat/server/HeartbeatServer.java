dccc5d94f40c83c0495bb8e24db42576fb3e9b62
Denis M. Gabaidulin <gabaden@gmail.com>
2011-09-04 01:46:34 +0400

    * remove akka, use jetty

---
+++ src/main/java/com/sherman/hearbeat/server/HeartbeatServer.java
@@ -0,0 +1,27 @@
+package com.sherman.hearbeat.server;
+
+import org.apache.log4j.Logger;
+import org.eclipse.jetty.server.Server;
+
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: sherman
+ * Date: 04.09.11
+ * Time: 1:32
+ * To change this template use File | Settings | File Templates.
+ */
+public class HeartbeatServer {
+    private static Logger log = Logger.getLogger(HeartbeatServer.class);
+
+    public static void main(String[] args) {
+        Server server = new Server(4433);
+        server.setHandler(new HearbeatHandler());
+        try {
+            server.start();
+            server.join();
+        } catch (Exception e) {
+            log.error(e);
+        }
+    }
+}
